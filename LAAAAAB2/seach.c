677c9881c598ffd3621b475b70c1a550533b7a7e
III-HJK-III <32160320@dankook.ac.kr>
2020-05-04 20:26:16 +0900

    serch 함수 반환값 변경
    기존 bool에서 노드 주소를 반환하도록 변경. 작동여부 확인 필요

--- LAAAAAB2/seach.c
+++ LAAAAAB2/seach.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include "BST.h"
 
-bool Search(BST *_Node, int value)
+Node* Search(BST *_Node, int value)
 {
 	Node *Value = _Node->root;
 	
 	while(1)
 	{
 		if(Value->key == value)        // if there is same value
-			return true;
+			return Value;
 			
 		if(Value->key < value)         // bigger
 		{
 			if(Value->r_child == NULL) // not exist
-				return false;
+				return NULL;
 			Value = Value->r_child;
 		}
 		
 		else                           // smaller
 		{
 			if(Value->l_child == NULL) // not exist
-				return false;
+				return NULL;
 			Value = Value->l_child;
 		}
 	}
