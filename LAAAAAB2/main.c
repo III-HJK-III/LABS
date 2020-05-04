d542320f9eed06a96e8c1f87ae92504c7070d884
III-HJK-III <32160320@dankook.ac.kr>
2020-05-05 01:46:34 +0900

    Update main.c
    1) printf 사용 수정
    2) thread 미사용으로 일단 main 완성 (동작여부 미확인)

--- LAAAAAB2/main.c
+++ LAAAAAB2/main.c
@@ -6,7 +6,11 @@ int main(int argc, char *argv[])
 {
     if(argc != 2 || )
     {
-        printf("[=]Usage[=]\n%s 0/1/2\n",argv[0]);
+        printf("[=]Usage[=]\n%s <option_number>\n",argv[0]);
+        printf("Options are : \n");
+        printf("0 : No Mutex\n");
+        printf("1 : With Coarse-grained Lock\n");
+        printf("2 : With Fine-grained Lock\n");
         assert(argc != 2);
     }
 
@@ -18,21 +22,27 @@ int main(int argc, char *argv[])
     switch(option){
         case 0: 
         //no mutex
+            printf("Without Mutex\n")
+            ins_f = insert;
+            rm_f = remove_Xmutex;
             break;
 
         case 1:
         // w/ coarse
+            printf("With Coarse-grained Lock\n")
             break;
 
         case 2:
         // w/ fine
+            printf("With Fine-grained Lock\n")
             break;
 
         default :
-            printf("[=]Usage[=]\n%s 0/1/2\n",argv[0]);
-            printf("0 : No Mutex\n
-                    1 : With Coarse-grained Lock\n
-                    2 : With Fine-grained Lock\n");
+            printf("[=]Usage[=]\n%s <option_number>\n",argv[0]);
+            printf("Options are : \n");
+            printf("0 : No Mutex\n");
+            printf("1 : With Coarse-grained Lock\n");
+            printf("2 : With Fine-grained Lock\n");
             assert( (option >= 0) && (option <= 2));
         }
 
@@ -41,15 +51,33 @@ int main(int argc, char *argv[])
     myArr input_key;
     init_myArr(input_key);
 
+    unsigned int cnt = 0;
+    unsigned int ran = 0;
+    srand((unsigned)time(NULL);)
     //making 'MAX_NODE' amount of nodes and insert into bst
+    printf("Starting Insert......\n")
+    printf("Creating %u nodes.......\n",MAX_NODE);
     while(1)
     {
-        //insert thing happens
+        ran = (((unsigned int)rand()<<15)|rand()) % (unsigned int)(MAX_NODE*4);
+        if( ins_f(&Sephiroth, ran) )
+            cnt++;
+        if(cnt >= MAX_NODE)
+            break;
     }
+    printf("Done Insert!\n")
 
-    //getting key_vals into input_keys array
-    assert( io_trav(Sephiroth, input_keys, MAX_NODE) );
+    //getting key_vals into input_keys array by inorder trav.
+    printf("Starting inorder traversal\n")
+    io_trav(Sephiroth, input_keys, MAX_NODE);
+    printf("Done inorder traversal\n")
 
-    
+    //tree deleting part, felling root
+    printf("Starting Deleting BST......\n")
+    while(Sephiroth->root)
+        rm_f(Sephiroth->root);
+    free(Sephiroth);
+    printf("Done Deleting BST!\n")
 
+    return 0;
 }
