f6ddbde4ee2e2dbbb2b17ba67f85952d6dc2be23
III-HJK-III <32160320@dankook.ac.kr>
2020-05-04 20:48:19 +0900

    Update remove.c
    노드 제거 함수인 remove를 완성

---
+++ LAAAAAB2/remove.c
@@ -0,0 +1,104 @@
+#include "bst.h"
+
+void remove_Xmutex(const Node &x)
+{
+    Node* p = root;//what 2 erase
+    Node* q = NULL;//p's parent
+
+    while(p)
+    {
+        //find place for x
+        if(p->key == x.key)
+            break; 
+
+        if(p->key < x.key)
+        {
+            q = p;
+            p = p->r_child;
+        }
+        else
+        {
+            q = p;
+            p = p->l_child;
+        }
+    }
+
+    if(!p)//not found
+    {
+        printf("Not Found!!\n");
+        return;
+    }
+
+    if(!p->l_child && !p->r_child)//no child
+    {
+        if(p == root)//if 'what 2 erase' is root
+            root = NULL;
+        else if(p == q->l_child)
+            q->l_child = NULL;
+        else
+            q->r_child = NULL;
+    }
+    else if(!p->r_child)//only 1 child
+    {
+        if(p == root)//if 'what 2 erase' is root
+        {
+            root = p->l_child;
+            p->l_child = NULL;
+        }
+        else if(p == q->l_child)
+        {
+            q->l_child = p->l_child;
+            p->l_child = NULL;
+        }
+        else
+        {
+            q->r_child = p->l_child;
+            p->l_child = NULL;
+        }
+    }
+    else if(!p->l_child)
+    {
+        if(p == root)//if 'what 2 erase' is root
+        {
+            root = p->r_child;
+            p->r_child = NULL;
+        }
+        else if(p == q->l_child)
+        {
+            q->l_child = p->r_child;
+            p->r_child = NULL;
+        }
+        else
+        {
+            q->r_child = p->r_child;
+            p->r_child = NULL;
+        }
+    }
+    else//have 2 child
+    {
+        Node* temp = p->l_child;
+        q = p;//now, q is used as parent of temp;
+
+        while(temp->r_child)
+        {
+            q = temp;
+            temp = temp ->r_child;
+        }
+        
+        if(q == p)//no r_child 4 1st temp
+        {
+            p->key = temp->key;
+            p->l_child = temp->l_child;
+            p = temp;//4 delete
+        }
+        else
+        {
+            p->key = temp->key;
+            q->r_child = NULL;
+            p = temp;//4 delete
+        }
+
+    }
+    free(p);
+    return;
+}
