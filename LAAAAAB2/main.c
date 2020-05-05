df58d3a57144c6911861220c6bf14332ec9c5acc
III-HJK-III <32160320@dankook.ac.kr>
2020-05-06 08:40:14 +0900

    Update main.c
    1) 함수 인자 참조 방법 수정
    2) 기존 노드 제거 방법 개선
    3) 변수의 이동(불필요한 부분이 있었기에)
    4) 조그만한 주석 수정
    5) 함수 포인터 선언 수정 & 함수 포인터 할당을 고침

--- LAAAAAB2/main.c
+++ LAAAAAB2/main.c
@@ -17,8 +17,8 @@ int main(int argc, char *argv[])
 
     int option = atoi(argv[1]);
 
-    bool (*ins_f)();//insert function
-    void (*rm_f)();//remove function
+    bool (*ins_f)(BST*,unsigned int);//insert function
+    bool (*rm_f)(BST*,unsigned int);//remove function
 
     switch(option){
         case 0: 
@@ -29,14 +29,14 @@ int main(int argc, char *argv[])
 
         case 1:
             printf("BST by Multi-Thread With Coarse-grained Lock\n")
-            ins_f = insert_Xmutex;
-            rm_f = remove_Xmutex;
+            ins_f = insert_CoarseLcok;
+            rm_f = remove_cg;
             break;
 
         case 2:
             printf("BST by Multi-Thread With Fine-grained Lock\n")
-            ins_f = insert_Xmutex;
-            rm_f = remove_Xmutex;
+            ins_f = insert_FineLock;
+            rm_f = remove_fg;
             break;
 
         case 3:
@@ -59,36 +59,37 @@ int main(int argc, char *argv[])
     init_tree(Sephiroth);
     myArr input_key;
     init_myArr(input_key);
+    myCounter myCnt;
 
     unsigned int *data = (unsigned int*)malloc(sizeof(unsigned int)*MAX_NODE);
     assert(data != NULL);
     rand_gen(data, MAX_NODE);
 
-    unsigned int i = 0;
-    unsigned int test = 0;
-
     //making 'MAX_NODE' amount of nodes and insert into bst
 
     if(option == 3)
     {
+        unsigned int i = 0;
+        unsigned int test = 0;
+
         printf("Starting Insert......\n")
         printf("Creating %u nodes.......\n",MAX_NODE);
         for(i = 0; i < MAX_NODE, i++)
         {
             test = ins_f(Sephiroth, data[i]);
-            if(!test) i--;
+            assert(!test);
         }
         printf("Done Insert!\n")
 
-        //getting key_vals into input_keys array by inorder trav.
+        //getting key_vals into input_keys array by inorder trav. instead of printing it out
         printf("Starting inorder traversal\n")
         io_trav(Sephiroth, input_keys, MAX_NODE);
         printf("Done inorder traversal\n")
 
         //tree deleting part, felling root
         printf("Starting Deleting BST......\n")
-        while(Sephiroth->root)
-            rm_f(Sephiroth->root);
+
+
         printf("Done Deleting BST!\n")
     }
     else
@@ -98,7 +99,7 @@ int main(int argc, char *argv[])
 
         printf("Done Insert!\n")
 
-        //getting key_vals into input_keys array by inorder trav.
+        //getting key_vals into input_keys array by inorder trav. instead of printing it out
         printf("Starting inorder traversal\n")
         io_trav(Sephiroth, input_keys, MAX_NODE);
         printf("Done inorder traversal\n")
