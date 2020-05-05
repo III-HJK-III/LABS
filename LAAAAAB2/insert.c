0cd7b3429d4df3ae7c85848755d27e2d43e40604
Quantumcat97 <64680580+Quantumcat97@users.noreply.github.com>
2020-05-06 03:06:49 +0900

    Update insert.c
    added Temp for CL
    changed some layouts

--- LAAAAAB2/insert.c
+++ LAAAAAB2/insert.c
@@ -1,47 +1,47 @@
-/*****************************************************/
+/********************************************************/
 /**********Made by 박경녕 from DKU, 32161553**********/
-/*****************************************************/ 
+/********************************************************/ 
 #include "bst.h"
 
 bool insert_Xmutex(BST *_Tree, unsigned int value)
 {
 	Node *NewNode = (Node *)malloc(sizeof(Node));
 
-	if(NewNode == NULL)                       //Exception
+	if(NewNode == NULL)                                //Exception
 		return FALSE;        
 					
-	init_node(NewNode, value);                   //make NewNode
+	init_node(NewNode, value);                         //make NewNode
 	
-	if (_Tree->root == NULL)                     //if root is empty
+	if (_Tree->root == NULL)                           //if root is empty
 	{
 		_Tree->root = NewNode;
 		return TRUE;
 	}
 	
-	if (Serch(value) == NULL)                    //what if value is already there?
+	if (Serch(value) == NULL)                          //what if value is already there?
 		return FALSE;
 
 
-	Node *Value = _Tree->root;                   //declare Value for value
+	Node *Value = _Tree->root;                         //declare Value for value
 
 	While(1)
 	{
-		if(Value->key < value)                   //if value is bigger
+		if(Value->key < value)                         //if value is bigger
 		{
-			if(Value->r_child == NULL)           //if r_child is empty
+			if(Value->r_child == NULL)                 //if r_child is empty
 			{
 				Value->r_child = NewNode;
 			}
-			Value = Value->r_child;              //if r_child is full
+			Value = Value->r_child;                    //if r_child is full
 		}
 		
-		else                                     //if value is smaller
+		else                                           //if value is smaller
 		{
-			if(Value->l_child == NULL)           //if l_child is empty
+			if(Value->l_child == NULL)                 //if l_child is empty
 			{
 				Value->l_child = NewNode; 
 			}
-			Value = Value->l_child;              //if l_child is full
+			Value = Value->l_child;                    //if l_child is full
 		}
 	}
 	return TRUE;
@@ -52,48 +52,47 @@ bool insert_CoarseLock(BST *_Tree, unsigned int value)
 {
 	Node *NewNode = (Node *)malloc(sizeof(Node));
 
-	if(NewNode == NULL)                       //Exception
+	if(NewNode == NULL)                                 //Exception
 		return FALSE;        
 					
-	init_node(NewNode, value);                   //make NewNode
+	init_node(NewNode, value);                          //make NewNode
 	
-	if (_Tree->root == NULL)                     //if root is empty
+	if (_Tree->root == NULL)                            //if root is empty
 	{
-		pthread_mutex_lock(&_Tree->treeLock);    //Lock _Tree
+		pthread_mutex_lock(&_Tree->treeLock);           //Lock _Tree
 		_Tree->root = NewNode;
-		pthread_mutex_unlock(&_Tree->TreeLock);  //Unlock
+		pthread_mutex_unlock(&_Tree->treeLock);         //Unlock
 		return TRUE;
 	}
 	
-	if (Serch(value) == NULL)                    //what if value is already there?
+	if (Serch(value) == NULL)                           //what if value is already there?
 		return FALSE;
 
-	pthread_mutex_lock(&Value->nodeLock);        //Lock Value
-	Node *Value = _Tree->root;                   //declare Value for value
-	pthread_mutex_unlock(&Value->nodeLock);      //Unlock
-
+	Node *Value = _Tree->root;                          //declare Value for value
+	Node *Temp == NULL;                                 //Temp for Lock
 
 	While(1)
 	{
-		pthread_mutex_lock(&Value->nodeLock);    //Lock Value
-		if(Value->key < value)                   //if value is bigger
+		pthread_mutex_lock(&Temp->nodeLock);            //Lock Temp
+		if(Value->key < value)                          //if value is bigger
 		{
-			if(Value->r_child == NULL)           //if r_child is empty
+			if(Value->r_child == NULL)                  //if r_child is empty
 			{
 				Value->r_child = NewNode;
 			}
-			Value = Value->r_child;              //if r_child is full
+			Temp = Value->r_child;                      //if r_child is full
 		}
 		
-		else                                     //if value is smaller
+		else                                            //if value is smaller
 		{
-			if(Value->l_child == NULL)           //if l_child is empty
+			if(Value->l_child == NULL)                  //if l_child is empty
 			{
 				Value->l_child = NewNode; 
 			}
-			Value = Value->l_child;              //if l_child is full
+			Value = Value->l_child;                     //if l_child is full
 		}
-		pthread_mutex_unlock(&Value->nodeLock);  //Unlock
+		pthread_mutex_unlock(&Temp->nodeLock);          //Unlock
+		Value = Temp;
 	}
 	return TRUE;
 }
@@ -103,52 +102,46 @@ bool insert_FineLock(BST *_Tree, unsigned int value)
 {
 	Node *NewNode = (Node *)malloc(sizeof(Node));
 
-	if(NewNode == NULL)                       //Exception
+	if(NewNode == NULL)                                  //Exception
 		return FALSE;        
 					
-	init_node(NewNode, value);                   //make NewNode
+	init_node(NewNode, value);                           //make NewNode
 	
-	if (_Tree->root == NULL)                     //if root is empty
+	if (_Tree->root == NULL)                             //if root is empty
 	{
-		pthread_mutex_lock(&_Tree->treeLock);    //Lock _Tree
+		pthread_mutex_lock(&_Tree->treeLock);            //Lock _Tree
 		_Tree->root = NewNode;
-		pthread_mutex_unlock(&_Tree->TreeLock);  //Unlock
+		pthread_mutex_unlock(&_Tree->treeLock);          //Unlock
 		return TRUE;
 	}
 	
-	if (Serch(value) == NULL)                    //what if value is already there?
+	if (Serch(value) == NULL)                            //what if value is already there?
 		return FALSE;
 
-	pthread_mutex_lock(&Value->nodeLock);        //Lock Value
-	Node *Value = _Tree->root;                   //declare Value for value
-	pthread_mutex_unlock(&Value->nodeLock);  //Unlock
-
+	Node *Value = _Tree->root;                           //declare Value for value
 
 	While(1)
 	{
-		if(Value->key < value)                   //if value is bigger
+		if(Value->key < value)                           //if value is bigger
 		{
-			if(Value->r_child == NULL)           //if r_child is empty
+			if(Value->r_child == NULL)                   //if r_child is empty
 			{
 				pthread_mutex_lock(&Value->nodeLock);    //Lock Value
 				Value->r_child = NewNode;
 				pthread_mutex_unlock(&Value->nodeLock);  //Unlock
 			}
-			pthread_mutex_lock(&Value->nodeLock);    //Lock Value
-			Value = Value->r_child;              //if r_child is full
-			pthread_mutex_unlock(&Value->nodeLock);  //Unlock
+			Value = Value->r_child;                      //if r_child is full
 		}
 		
-		else                                     //if value is smaller
+		else                                             //if value is smaller
 		{
-			if(Value->l_child == NULL)           //if l_child is empty
+			if(Value->l_child == NULL)                   //if l_child is empty
 			{
+				pthread_mutex_lock(&Value->nodeLock);    //Lock Value
 				Value->l_child = NewNode; 
 				pthread_mutex_unlock(&Value->nodeLock);  //Unlock
 			}
-			pthread_mutex_lock(&Value->nodeLock);    //Lock Value
-			Value = Value->l_child;              //if l_child is full
-			pthread_mutex_unlock(&Value->nodeLock);  //Unlock
+			Value = Value->l_child;                      //if l_child is full
 		}
 	}
 	return TRUE;
