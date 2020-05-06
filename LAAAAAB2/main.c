3a5dd5bf47f94e398f3f31dda3bf8aacf8da7941
III-HJK-III <32160320@dankook.ac.kr>
2020-05-06 13:44:19 +0900

    Update main.c
    코드 테스팅 용

--- LAAAAAB2/main.c
+++ LAAAAAB2/main.c
@@ -89,7 +89,7 @@ int main(int argc, char *argv[])
         for(i = 0; i < MAX_NODE; i++)
         {
             test = ins_f(&Sephiroth, data[i]);
-            assert(test);
+            //assert(test);
         }
         printf("Done Insert!\n");
 
@@ -104,7 +104,7 @@ int main(int argc, char *argv[])
         for(i = 0; i < MAX_NODE; i++)
         {
             test = rm_f(&Sephiroth, data[i]);
-            assert(test);
+            //assert(test);
         }
         printf("Done Deleting BST!\n");
     }
