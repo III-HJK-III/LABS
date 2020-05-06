d82ab273fe36ba6afe5fc3bf67e4464f9353e454
III-HJK-III <32160320@dankook.ac.kr>
2020-05-06 16:20:28 +0900

    Update bst.c
    debug

--- LAAAAAB2/bst.c
+++ LAAAAAB2/bst.c
@@ -92,6 +92,7 @@ void* lumberjack(void *arg)//destrory tree
     unsigned int i;
     unsigned int test;
     for(i=start ; i < end; i++ ){
+        print("%u !!!\n",i);
         test = remove(tree, data[i]);
         assert(test); // fail to remove one node -no node matching the input key-
     }
