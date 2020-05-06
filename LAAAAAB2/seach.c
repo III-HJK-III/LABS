7b681f5dad42d2e9c05a3ee778d6f3cb3e7e7e27
III-HJK-III <32160320@dankook.ac.kr>
2020-05-06 13:58:53 +0900

    Update seach.c
    search 함수 과정 수정

--- LAAAAAB2/seach.c
+++ LAAAAAB2/seach.c
@@ -4,26 +4,22 @@ Node* Search(BST *_Node, unsigned int value)
 {
 	Node *Value = _Node->root;
 	
-	if(!Value)
+	if(Value == NULL)
 		return NULL;
-		
-	while(1)
+
+	while(Value)
 	{
 		if(Value->key == value)        // if there is same value
-			return Value;
+			break;
 			
-		if(Value->key < value)         // bigger
+		else if(Value->key < value)         // bigger
 		{
-			if(Value->r_child == NULL) // not exist
-				return NULL;
 			Value = Value->r_child;
 		}
-		
 		else                           // smaller
 		{
-			if(Value->l_child == NULL) // not exist
-				return NULL;
 			Value = Value->l_child;
 		}
 	}
+	return Value;
 }
