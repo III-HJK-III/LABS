2b10b464a6f86c0d174b41a01fcbc87ea126b533
III-HJK-III <32160320@dankook.ac.kr>
2020-05-07 12:20:16 +0900

    Update insert.c
    다시 coarse fine 락 수정

--- LAAAAAB2/insert.c
+++ LAAAAAB2/insert.c
@@ -56,6 +56,64 @@ int insert_Xmutex(BST *_Tree, unsigned int value)
 
 int insert_CoarseLock(BST *_Tree, unsigned int value)
 {
+	pthread_mutex_lock(&Value->nodeLock);           //Lock Tree
+	Node *NewNode = (Node *)malloc(sizeof(Node));
+
+	if(NewNode == NULL)                                 //Exception
+	{
+		pthread_mutex_unlock(&_Tree->treeLock);         //Unlock
+		return FALSE;      
+	}  
+					
+	init_node(NewNode, value);                          //make NewNode
+
+	if (Search(_Tree ,value))                           //what if value is already there?
+	{
+		free(NewNode);
+		pthread_mutex_unlock(&_Tree->treeLock);         //Unlock
+		return FALSE;
+	}
+
+	if (_Tree->root == NULL)                            //if root is empty
+	{
+		_Tree->root = NewNode;
+		pthread_mutex_unlock(&_Tree->treeLock);         //Unlock
+		return TRUE;
+	}
+
+	Node *Value = _Tree->root;                          //declare Value for value
+	Node *Temp = NULL;                                  //Temp for Lock
+ 
+	while(1)
+	{
+		if(Value->key < value)                          //if value is bigger
+		{
+			if(Value->r_child == NULL)                  //if r_child is empty
+			{
+				Value->r_child = NewNode;
+				break;
+			}
+			Temp = Value->r_child;                      //if r_child is full
+		}
+		
+		else                                            //if value is smaller
+		{
+			if(Value->l_child == NULL)                  //if l_child is empty
+			{
+				Value->l_child = NewNode; 
+				break;
+			}
+			Temp = Value->l_child;                      //if l_child is full
+		}
+		Value = Temp;
+	}
+	pthread_mutex_unlock(&Value->nodeLock);         //Unlock
+	return TRUE;
+}
+
+
+int insert_FineLock(BST *_Tree, unsigned int value)
+{
 	Node *NewNode = (Node *)malloc(sizeof(Node));
 
 	if(NewNode == NULL)                                 //Exception
@@ -107,57 +165,5 @@ int insert_CoarseLock(BST *_Tree, unsigned int value)
 		pthread_mutex_unlock(&Value->nodeLock);         //Unlock
 		Value = Temp;
 	}
-	return TRUE;
-}
-
-
-int insert_FineLock(BST *_Tree, unsigned int value)
-{
-	Node *NewNode = (Node *)malloc(sizeof(Node));
-	Node *Value = _Tree->root;                           //declare Value for value
-
-	if(NewNode == NULL)                                  //Exception
-		return FALSE;        
-					
-	init_node(NewNode, value);                           //make NewNode
-
-	if (Search(_Tree ,value))                            //what if value is already there?
-	{
-		free(NewNode);
-		return FALSE;
-	}
-
-	int tmp = TRUE;
-	while(_Tree->root && tmp )
-	{
-		pthread_mutex_lock(&Value->nodeLock);    //Lock Value
-		if(Value->key < value)                           //if value is bigger
-		{
-			if(Value->r_child == NULL)                   //if r_child is empty
-			{
-				Value->r_child = NewNode;
-				tmp = FALSE;
-			}
-			Value = Value->r_child;                      //if r_child is full
-		}
-		else                                             //if value is smaller
-		{
-			if(Value->l_child == NULL)                   //if l_child is empty
-			{
-				Value->l_child = NewNode; 
-				tmp = FALSE;
-			}
-			Value = Value->l_child;                      //if l_child is full
-		}
-		pthread_mutex_unlock(&Value->nodeLock);  //Unlock
-	}
-
-	pthread_mutex_lock(&_Tree->treeLock);            //Lock _Tree
-	if (_Tree->root == NULL)                             //if root is empty
-	{
-		_Tree->root = NewNode;
-	}
-	pthread_mutex_unlock(&_Tree->treeLock);          //Unlock
-
 	return TRUE;
 }
