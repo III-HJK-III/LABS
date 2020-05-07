30714cdc76c0847808081f83df115d9d0afcf335
III-HJK-III <32160320@dankook.ac.kr>
2020-05-07 12:10:04 +0900

    Update insert.c
    fine grained 부분 timer lock 개선

--- LAAAAAB2/insert.c
+++ LAAAAAB2/insert.c
@@ -114,6 +114,7 @@ int insert_CoarseLock(BST *_Tree, unsigned int value)
 int insert_FineLock(BST *_Tree, unsigned int value)
 {
 	Node *NewNode = (Node *)malloc(sizeof(Node));
+	Node *Value = _Tree->root;                           //declare Value for value
 
 	if(NewNode == NULL)                                  //Exception
 		return FALSE;        
@@ -126,41 +127,37 @@ int insert_FineLock(BST *_Tree, unsigned int value)
 		return FALSE;
 	}
 
+	pthread_mutex_lock(&_Tree->treeLock);            //Lock _Tree
 	if (_Tree->root == NULL)                             //if root is empty
 	{
-		pthread_mutex_lock(&_Tree->treeLock);            //Lock _Tree
 		_Tree->root = NewNode;
-		pthread_mutex_unlock(&_Tree->treeLock);          //Unlock
-		return TRUE;
 	}
+	pthread_mutex_unlock(&_Tree->treeLock);          //Unlock
 
-	Node *Value = _Tree->root;                           //declare Value for value
 
-	while(1)
+	while(_Tree->root && tmp )
 	{
+		pthread_mutex_lock(&Value->nodeLock);    //Lock Value
 		if(Value->key < value)                           //if value is bigger
 		{
 			if(Value->r_child == NULL)                   //if r_child is empty
 			{
-				pthread_mutex_lock(&Value->nodeLock);    //Lock Value
 				Value->r_child = NewNode;
-				pthread_mutex_unlock(&Value->nodeLock);  //Unlock
-				break;
+				tmp = FALSE;
 			}
 			Value = Value->r_child;                      //if r_child is full
 		}
-		
 		else                                             //if value is smaller
 		{
 			if(Value->l_child == NULL)                   //if l_child is empty
 			{
-				pthread_mutex_lock(&Value->nodeLock);    //Lock Value
 				Value->l_child = NewNode; 
-				pthread_mutex_unlock(&Value->nodeLock);  //Unlock
-				break;
+				tmp = FALSE;
 			}
 			Value = Value->l_child;                      //if l_child is full
 		}
+		pthread_mutex_unlock(&Value->nodeLock);  //Unlock
 	}
+
 	return TRUE;
 }
